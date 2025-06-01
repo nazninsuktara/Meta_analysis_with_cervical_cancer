d51fc421e4f1ffd482d58fac57d33d377524b586
nazninsuktara <nazninsuktara@gmail.com>
2025-06-01 11:49:12 +0600

    added data preprocessing

---
+++ scripts/data_preprocessing.R
@@ -0,0 +1,7 @@
+#Identification of Drug-Responsive Gene Signatures in Cervical Cancer: A Microarray Meta-Analysis(GSE3578, GSE7803, GSE9750, GSE63514)
+
+#install packages
+install.packages("BiocManager")
+BiocManager::install("GEOquery")
+#load packages
+library(GEOquery)
